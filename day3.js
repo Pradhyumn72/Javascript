5a6cd53a71197629dc60196b93efeb0783ea87b0
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-03-30 15:01:03 +0530

    updated

---
+++ day3.js
@@ -0,0 +1,35 @@
+// var a =17;
+// var b = 9;
+// console.log("the sum of " + a + " and " + b + " is " +(a+b));
+
+
+// function intro(){
+//     var a =7;
+//     var b=2;
+//     console.log(a+b);
+//     console.log(typeof(a));
+//     console.log(typeof(b));
+//     console.log(a-b);
+//     console.log(a*b);
+//     console.log(a/b);
+// }
+// intro();
+
+// function my(){
+//     console
+// }
+
+
+
+// redeclaration possible in var but not in let&const
+// reassignment possible in var & let but not in const
+var a=6;
+a=7;
+let c=9;
+c=5;
+// const d =4;
+// d=2;
+
+console.log(a);
+console.log(c);
+console.log(d);
