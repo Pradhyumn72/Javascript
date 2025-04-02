69b26f162553470babaa9d0f97ac74c5af4986cd
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-02 18:31:22 +0530

    updated

---
+++ day6.js
@@ -0,0 +1,23 @@
+let a =" This is string 1 ";
+let b =" Hellow str 2 ";
+// string methods
+// length(): len of string
+// slice(): elements b/w two indexes of str 
+//split(): converts str to array
+// replace();replaces text wiht str
+// concat(): combines text of 2 str
+//substring(): returns elements b/w two strings 
+//toLowerCase():
+// toUpperCase()
+//trim; removes leading and traiiling white space and line terminator char
+
+console.log("<br>"+a.length);
+console.log("<br>"+a[2]+ b[5]);
+document.write("<br>"+a + b); // concatanation
+document.write("<br>"+b.toUpperCase());
+document.write("<br>"+b.toLowerCase());
+// document.write("<br>"+ b.charAt(5));
+// document.write("<br>" + a.charCodeAt(4))//ASCII value
+
+document.write("<br>"+a.slice(0,5)) // slice
+
