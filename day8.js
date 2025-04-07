c32f778bc8184d0480676554e2d84857e1265aa2
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-07 17:43:44 +0530

    updated

--- day8.js
+++ day8.js
@@ -1,36 +1,45 @@
 // let fruits =["Mango", "Apples",27,"Grapes",72,"Banana"];
 
-// methods
-// length :no of elements in arrray
-// pop:remove last element
-//push: add new element to end
-// shift:removes first element
-//unshift: add new element to beginning
+// // methods
+// // length :no of elements in arrray
+// // pop:remove last element
+// //push: add new element to end
+// // shift:removes first element
+// //unshift: add new element to beginning
 
-//splice helps to add elements wherever u want to
-
-
-
-// console.log(fruits[2]);// accessing array
-// console.log(fruits.length);
+// //splice helps to add elements wherever u want to
 
 
+// // console.log(fruits[2]);// accessing array
+// // console.log(fruits.length);
 
-// fruits.pop();
-// document.write(fruits);
+
+// // fruits.pop();
+// // document.write(fruits);
 
 
-// fruits.push("Guava");
+// // fruits.push("Guava");
+// // console.log(fruits);
+
+// // fruits.unshift("Kiwi");
+// // console.log(fruits);
+
+// // fruits.shift();
+// // console.log(fruits);
+
+
+// let sport=["cricket","football","golf"];
+// let equipment=["bat","studs","stick"];
+// let venue=["pitch","field","course"];
+
+// // console.log(sport.concat(equipment,venue ));
+
+
+// console.log(fruits.slice(1,4));
+// fruits.splice(1,0,"pine") // from where to add, how many to delete,what to do
 // console.log(fruits);
 
-// fruits.unshift("Kiwi");
-// console.log(fruits);
 
-// fruits.shift();
-// console.log(fruits);
-
-
-let sport=["cricket","football","golf"];
-let equipment=["bat","studs","stick"];
-let venue=["pitch","field","course"];
-console.log(sport.concat(equipment,venue ));
+let arr=[2,5,1,0,4,6,6,2,2,3,3];
+console.log(arr.sort());
+console.log(arr.reverse());
