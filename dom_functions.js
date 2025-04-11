366c62d785ab0b55354e784365cd699e945aead0
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-11 17:47:37 +0530

    updated

---
+++ dom_functions.js
@@ -0,0 +1,58 @@
+// document.getElementById("para")
+
+// for class
+// document.getElementsByClassName("")
+
+// document.querySelector("# for id and . for class") // we can pick class as well as id
+
+// function okk(){
+// // let show = document.querySelector("#para")
+// // show.innerHTML="book"
+// // for one line only 
+// let show = document.querySelector("#para")
+// // for giving css
+// show.innerHTML="boooooooksss"
+// show.style.color="red"
+// }
+
+
+// // ===============Anonymous Function===============
+// // initialize a variable with a function 
+// let hi=function(){
+//     console.log("Anonymous Function");
+// }
+// hi();
+
+// //===============IIFE===============
+// (
+//     function(){
+//         console.log("IIFE");
+//     }
+// )()
+
+
+// ===============Arrow function("=>")===============
+// also called special type of anonymous function
+
+// let bunny=()=>{
+//     console.log("Arrow Function");
+// }
+// bunny()
+let o1=document.querySelector(".para")
+let orange=()=>{
+        o1.style.backgroundColor="orange";
+}
+
+let green=()=>{
+    
+    o1.style.backgroundColor=("green");
+}
+let reset=()=>{
+    o1.style.backgroundColor=("grey")
+}
+let img=()=>{
+    o1.style.backgroundImage='url(./img1.jpg)'
+}
+o1.src="image path"; // to change img
+
+
