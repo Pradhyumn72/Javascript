85225d16c5b25edee22c9fcf9095c55662d887e4
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-15 17:48:49 +0530

    updated

---
+++ object.js
@@ -0,0 +1,16 @@
+let text="this is a string"
+let fruits=["Bunny",25,"Bhopal"];
+
+let obj ={
+    // key:value
+    Name:"Bunny",
+    Age:25,
+    City:"Bhopal"
+
+}
+console.log(obj); //to print full obj
+console.log(obj.Age)// to print any specific key 
+obj.college="VIT"; // to insert an object
+console.log(obj)
+obj.Age=28;
+console.log(obj);
