3584311d73e0a7113358f19e6ae278ef1d7a12c0
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-04-15 18:05:25 +0530

    updated

--- object.js
+++ object.js
@@ -1,20 +1,29 @@
-let text="this is a string"
-let fruits=["Bunny",25,"Bhopal"];
+// let text="this is a string"
+// let fruits=["Bunny",25,"Bhopal"];
 
-let obj ={
-    // key:value
-    Name:"Bunny",
-    Age:25,
-    City:"Bhopal"
+// let obj ={
+//     // key:value
+//     Name:"Bunny",
+//     Age:25,
+//     City:"Bhopal"
 
+// }
+// console.log(obj); //to print full obj
+// console.log(obj.Age)// to print any specific key 
+// obj.college="VIT"; // to insert an object
+// console.log(obj)
+// obj.Age=28; // editing the existing key
+// console.log(obj);
+// delete obj.City; // for deleting the object
+// console.log(obj)
+
+// destrucutring 
+const student={
+    name:"john",
+    age:20,
+    city:"indore"
 }
-console.log(obj); //to print full obj
-console.log(obj.Age)// to print any specific key 
-obj.college="VIT"; // to insert an object
-console.log(obj)
-obj.Age=28; // editing the existing key
-console.log(obj);
-delete obj.City; // for deleting the object
-console.log(obj)
 
-
+const {name,age} = student;
+console.log(name);
+console.log(age);
