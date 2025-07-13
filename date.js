a0ccf1d74245becc5a7358360be67e2d42d1446c
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-07-13 22:14:37 +0530

    updated

--- date.js
+++ date.js
@@ -5,7 +5,7 @@ setInterval(() => {
     let Mydate=new  Date()
 let hours=document.querySelector("#hours");
     hours.innerHTML=`${Mydate.getHours()} hours ${Mydate.getMinutes()}minutes ${Mydate.getSeconds()} seconds   ${Mydate.getMilliseconds()} ms`
-}, 1000);
+}, 2000);
 // a =console.log(1000+Math.round(Math.random()*9000));
 
 
